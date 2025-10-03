8f2d3b63ad4350b72afccbd690278f5a32c598de
bkluszynski <kluszynski.benjamin@gmail.com>
2025-10-03 08:16:35 +0200

    Create create.sh

---
+++ create.sh
@@ -0,0 +1,4 @@
+cd /opt
+sudo git clone https://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@github.com/bkluszynski/walkingcows.de.git
+cd walkingcows.de
+docker compose up -d
